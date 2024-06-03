297ebccfd67cf4dab3d0838865297b71d8ab93bb
Nikash M.S. <nikash8e2@gmail.com>
2024-06-03 15:28:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,20 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.js',
-  });
-  
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+});
+
 /**
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
-  basePath: "/docs",
-  output: 'export',
-  reactStrictMode: true,
+basePath: "/docs",
+output: 'export',
+reactStrictMode: true,
 };
 
 module.exports = {
-  ...withNextra(),
-  basePath: '/docs',
-  output: 'export',
-  images: { unoptimized: true },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'introduction',
-        permanent: true,
-      },
-    ];
-  },
+...withNextra(),
+basePath: '/docs',
+output: 'export',
+images: { unoptimized: true },
 }
