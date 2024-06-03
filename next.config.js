b573b8cab8cd8522ecd98d0d2393d4308d264359
Nikash M.S. <nikash8e2@gmail.com>
2024-06-03 17:08:54 +0530

    New Bug Fixes

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.js',
+  });
+  
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  basePath: "/docs",
+  output: 'export',
+  reactStrictMode: true,
+};
+
+module.exports = {
+  ...withNextra(),
+  basePath: '/docs',
+  output: 'export',
+  images: { unoptimized: true },
+}
